72009d84fcaee490f906b1f58c4df44a8672a94d
LucasDev96 <lucaslong96@gmail.com>
2023-01-14 18:15:20 -0500

    Update To Main Menu Text Functionality
    The highscore text now automatically updates whenever it is loaded. The name input field now also refills with the player's name if they returned to the main menu from the game scene.

---
+++ Assets/Scripts/MainMenuText.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class MainMenuText : MonoBehaviour
+{
+    public TMP_Text highScoreText;
+    public TMP_InputField nameInput;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        SetHighScoreText();
+        RefillNameInputField();
+    }
+
+    // Set the high score name and text
+    void SetHighScoreText()
+    {
+        highScoreText.text = $"High Score:\n{DataManager.Instance.highScoreName} - {DataManager.Instance.highScore}";
+    }
+
+    // If returning from the game scene, fill the player's name back into the input field
+    void RefillNameInputField()
+    {
+        if (DataManager.Instance.playerName != null) nameInput.text = DataManager.Instance.playerName;
+    }
+}

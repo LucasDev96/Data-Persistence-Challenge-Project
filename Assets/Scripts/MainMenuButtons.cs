4ac156c9c4e496f53138348f6e77c9a04eb8107c
LucasDev96 <lucaslong96@gmail.com>
2023-01-14 14:35:34 -0500

    Set playerName On Game Start
    Assign the playerName in the DataManager when the start button is pressed.

--- Assets/Scripts/MainMenuButtons.cs
+++ Assets/Scripts/MainMenuButtons.cs
@@ -1,15 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEditor;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class MainMenuButtons : MonoBehaviour
 {
+    public TMP_InputField nameInput;
+
     // Go to the main game after pressing the start button
     public void StartGame()
     {
+        AssignPlayerName();
+
         SceneManager.LoadScene(1, LoadSceneMode.Single);
+    }
+
+    // Use the text input field to assign the name in the DataManager
+    private void AssignPlayerName()
+    {
+        if (nameInput.text.Length < 1)
+        {
+            DataManager.Instance.playerName = "name";
+        }
+        else
+        {
+            DataManager.Instance.playerName = nameInput.text;
+        }
     }
 
     // Exit the application, or play mode if in the editor
